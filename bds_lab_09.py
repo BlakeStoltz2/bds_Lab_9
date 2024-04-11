c33a9c55340f59694973ba255212297990a90d02
BlakeStoltz <blakestoltz@icloud.com>
2024-04-10 21:35:43 -0400

    bds encode and main function

--- bds_lab_09.py
+++ bds_lab_09.py
@@ -1,38 +1,38 @@
-# BDS encode function
+# bds encoder function
 def encode(password):
-    encoded_password = ''
+    encoded_password = ""
     for digit in password:
+        # Shift each digit up by 3
         encoded_digit = str((int(digit) + 3) % 10)
         encoded_password += encoded_digit
     return encoded_password
 
-# decode function
+# decoder function
 def decode(encoded_password):
 
 
-# BDS main function
+# bds main function
 def main():
     while True:
-        print("Hello its me")
-        print("1. Encode Password")
-        print("2. Decode Password")
-        print("3. Exit")
-        choice = input("Enter your choice: ")
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        option = input("Please enter an option: ")
 
-        if choice == '1':
-            password = input("Enter password to encode: ")
-            print("Encoded password:", encode(password))
-
-        elif choice == '2':
-            encoded_password = input("Enter password to decode: ")
-            print("Decoded password:", decode(encoded_password))
-
-        elif choice == '3':
+        if option == "1":
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!")
+        elif option == "2":
+            encoded_password = input("Please enter the encoded password: ")
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+        elif option == "3":
             break
-
         else:
-            print("Invalid choice!")
-
+            print("Invalid option. Please try again.")
 
 if __name__ == "__main__":
     main()
